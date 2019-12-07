3aa6a8123d2191b802ce8c421be88fead1eb4471
Junior Alves <jjalves96@gmail.com>
2019-12-07 11:00:22 -0300

    Chore: User´s index

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -12,6 +12,12 @@ class UserController {
 
     return res.json(user);
   }
+
+  async index(req, res) {
+    const users = await User.findAll();
+
+    return res.json(users);
+  }
 }
 
 export default new UserController();
